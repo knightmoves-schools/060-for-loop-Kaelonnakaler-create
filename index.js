8d3871032653558de780e417533b5ff7ce6cf00a
kaelonnakaler-create <kaelonna.kaler@students.msswarriors.org>
2026-02-25 13:24:47 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
-function markAsDone = ['todos]; 
-returns todos
+function markAsDone = (todos){ 
+  for (let i = 0; i < todos.length; i++) {
+    todos[i] = 'done - ' + todos[i]
+  }
+return todos;
+}
                       
+
