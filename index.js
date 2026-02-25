f0490076a410e7012c1f41b6071bb74c59951b03
kaelonnakaler-create <kaelonna.kaler@students.msswarriors.org>
2026-02-25 13:31:01 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 function markAsDone = (todos){ 
-  let todos = ['Red', 'Green', 'Blue']
+  let todos = ['Red', 'Blue', 'Green'];
   for (let i = 0; i < todos.length; i++) {
     todos[i] = 'done - ' + todos[i]
   }
 return todos;
 }
                       
+
 
 
